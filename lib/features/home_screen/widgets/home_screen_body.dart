4d764c3767267914d8ea932304dd487f91953132
unknown <rama.nassif.4@gmail.com>
2023-03-21 14:24:04 +0300

    Add Drawer

--- lib/features/home_screen/widgets/home_screen_body.dart
+++ lib/features/home_screen/widgets/home_screen_body.dart
@@ -3,7 +3,7 @@ import 'package:book_flight_app/core/services/theme_service/theme_service.dart';
 import 'package:book_flight_app/features/home_screen/widgets/flight_selection_widget.dart';
 import 'package:book_flight_app/features/home_screen/widgets/offer_section.dart';
 import 'package:book_flight_app/features/home_screen/widgets/way_selection_widget.dart';
-import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization/easy_localization.dart' hide TextDirection;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,63 +15,38 @@ class HomeScreenBody extends StatefulWidget {
 }
 
 class _HomeScreenBodyState extends State<HomeScreenBody> {
+  String? currentLanguage;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor:
-            Provider.of<ThemeServices>(context).mode == ThemeMode.dark
-                ? kDarkBackgroundColor
-                : kLightBackgroundColor,
-        centerTitle: true,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.only(
-            left: 24.0,
-            top: 8,
-          ),
-          child: GestureDetector(
-            onTap: () {
-              Provider.of<ThemeServices>(context, listen: false).toggleMode();
-              setState(() {});
-            },
-            child: Icon(
+    return Directionality(
+      textDirection:
+          (currentLanguage == 'en_EN') ? TextDirection.ltr : TextDirection.rtl,
+      child: Scaffold(
+        endDrawer: const Drawer(
+          backgroundColor: kPrimaryColor,
+        ),
+        appBar: AppBar(
+          backgroundColor:
               Provider.of<ThemeServices>(context).mode == ThemeMode.dark
-                  ? Icons.wb_sunny_outlined
-                  : Icons.nightlight_round_outlined,
-              color: Provider.of<ThemeServices>(context).mode == ThemeMode.dark
-                  ? kLightColor
-                  : kDarkColor,
+                  ? kDarkBackgroundColor
+                  : kLightBackgroundColor,
+          centerTitle: true,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.only(
+              left: 24.0,
+              top: 8,
             ),
-          ),
-        ),
-        title: Padding(
-          padding: const EdgeInsets.only(
-            top: 24.0,
-            bottom: 12.0,
-          ),
-          child: Text(
-            'Book_Flight'.tr().toString(),
-            style: TextStyle(
-              fontSize: 20,
-              color: Provider.of<ThemeServices>(context).mode == ThemeMode.dark
-                  ? kLightColor
-                  : kDarkColor,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Padding(
-              padding: const EdgeInsets.only(
-                right: 24.0,
-                bottom: 22,
-                top: 24.0,
-              ),
+            child: GestureDetector(
+              onTap: () {
+                Provider.of<ThemeServices>(context, listen: false).toggleMode();
+                setState(() {});
+              },
               child: Icon(
-                Icons.menu,
+                Provider.of<ThemeServices>(context).mode == ThemeMode.dark
+                    ? Icons.wb_sunny_outlined
+                    : Icons.nightlight_round_outlined,
                 color:
                     Provider.of<ThemeServices>(context).mode == ThemeMode.dark
                         ? kLightColor
@@ -79,39 +54,88 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
               ),
             ),
           ),
-        ],
-      ),
-      body: Container(
-        color: Provider.of<ThemeServices>(context).mode == ThemeMode.dark
-            ? kDarkBackgroundColor
-            : kLightBackgroundColor,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const WaySelectionWidget(),
-              const FlightSelectionWidget(),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 16.0,
-                  right: 16.0,
-                  bottom: 16.0,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 2,
-                  color:
-                      Provider.of<ThemeServices>(context).mode == ThemeMode.dark
-                          ? Colors.grey
-                          : kContainerBorderColor,
-                ),
+          title: Padding(
+            padding: const EdgeInsets.only(
+              top: 24.0,
+              bottom: 12.0,
+            ),
+            child: Text(
+              'Book_Flight'.tr().toString(),
+              style: TextStyle(
+                fontSize: 20,
+                color:
+                    Provider.of<ThemeServices>(context).mode == ThemeMode.dark
+                        ? kLightColor
+                        : kDarkColor,
+                fontWeight: FontWeight.w500,
               ),
-              const OfferSection(),
-            ],
+            ),
+          ),
+          actions: [
+            Builder(builder: (context) {
+              return GestureDetector(
+                onTap: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    right: 24.0,
+                    bottom: 22,
+                    top: 24.0,
+                  ),
+                  child: Icon(
+                    Icons.menu,
+                    color: Provider.of<ThemeServices>(context).mode ==
+                            ThemeMode.dark
+                        ? kLightColor
+                        : kDarkColor,
+                  ),
+                ),
+              );
+            }),
+          ],
+        ),
+        body: Container(
+          color: Provider.of<ThemeServices>(context).mode == ThemeMode.dark
+              ? kDarkBackgroundColor
+              : kLightBackgroundColor,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const WaySelectionWidget(),
+                const FlightSelectionWidget(),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16.0,
+                    right: 16.0,
+                    bottom: 16.0,
+                  ),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 2,
+                    color: Provider.of<ThemeServices>(context).mode ==
+                            ThemeMode.dark
+                        ? Colors.grey
+                        : kContainerBorderColor,
+                  ),
+                ),
+                const OfferSection(),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void didChangeDependencies() {
+    Locale myLocale = Localizations.localeOf(context);
+    currentLanguage = myLocale.toString();
+    debugPrint('$myLocale'.toString());
+    debugPrint(currentLanguage);
+    super.didChangeDependencies();
   }
 }
