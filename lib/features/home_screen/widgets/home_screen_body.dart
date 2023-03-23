ffa020969d8361943aa137df18dfbb499657bec0
unknown <rama.nassif.4@gmail.com>
2023-03-23 21:48:51 +0300

    Add Change app theme and Change app language to drawer

--- lib/features/home_screen/widgets/home_screen_body.dart
+++ lib/features/home_screen/widgets/home_screen_body.dart
@@ -4,6 +4,8 @@ import 'package:book_flight_app/features/home_screen/widgets/flight_selection_wi
 import 'package:book_flight_app/features/home_screen/widgets/offer_section.dart';
 import 'package:book_flight_app/features/home_screen/widgets/way_selection_widget.dart';
 import 'package:easy_localization/easy_localization.dart' hide TextDirection;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,6 +18,8 @@ class HomeScreenBody extends StatefulWidget {
 
 class _HomeScreenBodyState extends State<HomeScreenBody> {
   String? currentLanguage;
+  bool isSwitchedTheme = false;
+  bool isSwitchedLanguage = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +27,104 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
       textDirection:
           (currentLanguage == 'en_EN') ? TextDirection.ltr : TextDirection.rtl,
       child: Scaffold(
-        endDrawer: const Drawer(
-          backgroundColor: kPrimaryColor,
+        endDrawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 24.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Choose Your Theme'),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    const Text('Light Mode'),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Icon(
+                      Icons.wb_sunny_outlined,
+                      color: Provider.of<ThemeServices>(context).mode ==
+                              ThemeMode.dark
+                          ? kLightColor
+                          : kDarkColor,
+                    ),
+                    Transform.scale(
+                      scale: 0.8,
+                      child: CupertinoSwitch(
+                        activeColor: kPrimaryColor,
+                        dragStartBehavior: DragStartBehavior.start,
+                        value: isSwitchedTheme,
+                        onChanged: (value) {
+                          if (isSwitchedTheme == false) {
+                            setState(() {
+                              isSwitchedTheme = true;
+                              Provider.of<ThemeServices>(context, listen: false)
+                                  .toggleMode();
+                            });
+                          } else {
+                            setState(() {
+                              isSwitchedTheme = false;
+                              Provider.of<ThemeServices>(context, listen: false)
+                                  .toggleMode();
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                    const Text('Dark Mode'),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Icon(
+                      Icons.nightlight_round_outlined,
+                      color: Provider.of<ThemeServices>(context).mode ==
+                              ThemeMode.dark
+                          ? kLightColor
+                          : kDarkColor,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text('Choose App Language'),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Text('English'),
+                    Transform.scale(
+                      scale: 0.8,
+                      child: CupertinoSwitch(
+                        activeColor: kPrimaryColor,
+                        dragStartBehavior: DragStartBehavior.start,
+                        value: isSwitchedLanguage,
+                        onChanged: (value) {
+                          if (isSwitchedLanguage == false) {
+                            setState(() {
+                              isSwitchedLanguage = true;
+                            });
+                          } else {
+                            setState(() {
+                              isSwitchedLanguage = false;
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                    const Text('Arabic'),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
         appBar: AppBar(
           backgroundColor:
@@ -33,27 +133,6 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
                   : kLightBackgroundColor,
           centerTitle: true,
           elevation: 0,
-          leading: Padding(
-            padding: const EdgeInsets.only(
-              left: 24.0,
-              top: 8,
-            ),
-            child: GestureDetector(
-              onTap: () {
-                Provider.of<ThemeServices>(context, listen: false).toggleMode();
-                setState(() {});
-              },
-              child: Icon(
-                Provider.of<ThemeServices>(context).mode == ThemeMode.dark
-                    ? Icons.wb_sunny_outlined
-                    : Icons.nightlight_round_outlined,
-                color:
-                    Provider.of<ThemeServices>(context).mode == ThemeMode.dark
-                        ? kLightColor
-                        : kDarkColor,
-              ),
-            ),
-          ),
           title: Padding(
             padding: const EdgeInsets.only(
               top: 24.0,
